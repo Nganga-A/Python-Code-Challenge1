feb9fca86ee57311419a7b7c47a7b1d9164c472c
Abed Nganga <ngangaabed@gmail.com>
2023-08-20 00:16:21 +0300

    Completed Challenge3

---
+++ lib/challenge3.py
@@ -0,0 +1,19 @@
+def solve(s):
+    consonants = "bcdfghjklmnpqrstvwxyz"
+    max_value = 0
+    current_value = 0
+
+    for char in s:
+        if char in consonants:
+            current_value += ((ord(char) - ord('a')) + 1)  
+            #calculates the offset between the Unicode code points of the given character 
+            # and the lowercase letter 'a'. This offset represents the position of the character in the English alphabet. 
+            max_value = max(max_value, current_value)
+        else:
+            current_value = 0 #if character is not a consonant
+
+    return max_value
+
+# Test cases
+print(solve("zodiacs"))   # Output: 26
+print(solve("strength"))  # Output: 57
