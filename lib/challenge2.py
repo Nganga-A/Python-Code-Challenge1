e6e5bb512df9228cd45e6f0142fe741a3fc78117
Abed Nganga <ngangaabed@gmail.com>
2023-08-20 11:51:25 +0300

    Added User Input Feature

--- lib/challenge2.py
+++ lib/challenge2.py
@@ -10,6 +10,19 @@ def exactly_two_positive(a, b, c):
 
     return positive_count == 2  #returns True if positive_count is equal to 2  and False if otherwise.
 
+
+# Get user input
+input_a = int(input("Enter the value for a: "))
+input_b = int(input("Enter the value for b: "))
+input_c = int(input("Enter the value for c: "))
+
+# Call the function with user input
+result = exactly_two_positive(input_a, input_b, input_c)
+
+# Display the output
+print("Exactly two of the numbers are positive:", result)
+
+
 # Test cases
-print(exactly_two_positive(2, 4, -3))    # Output: True
-print(exactly_two_positive(-14, -3, -4)) # Output: False
+# print(exactly_two_positive(2, 4, -3))    # Output: True
+# print(exactly_two_positive(-14, -3, -4)) # Output: False
