ae314f5b525f3d932651cad48d88648a84d654ca
Abed Nganga <ngangaabed@gmail.com>
2023-08-20 11:48:01 +0300

    Added User Input Feature

--- lib/challenge1.py
+++ lib/challenge1.py
@@ -12,6 +12,20 @@ def convert_to_24_hour(hour, minute, period):
     
     return f"{hour_24:02d}{minute:02d}" #2 decimal point with optional 0 at the beginning
 
+# Get user input
+input_hour = int(input("Enter the hour: "))
+input_minute = int(input("Enter the minute: "))
+input_period = input("Enter 'am' or 'pm': ").lower()
+
+# Call the function with user input
+converted_time = convert_to_24_hour(input_hour, input_minute, input_period)
+
+# Display the output
+print("Converted time in 24-hour format:", converted_time)
+
+
+
+
 # Test cases
-print(convert_to_24_hour(8, 30, "am"))  # Output: "0830"
-print(convert_to_24_hour(8, 30, "pm"))  # Output: "2030"
+# print(convert_to_24_hour(8, 30, "am"))  # Output: "0830"
+# print(convert_to_24_hour(8, 30, "pm"))  # Output: "2030"
